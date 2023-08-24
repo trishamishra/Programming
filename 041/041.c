f20ed94bb5bfaff9bacba47c9e0c2a69de1a42ed
trishamishra <trishamishra2007@gmail.com>
2023-08-24 16:34:35 +0530

    Update the Respository

---
+++ 041/041.c
@@ -0,0 +1,19 @@
+/*
+
+1)
+Enter a non-negative integer: 1234
+The reverse of 1234 is 4321
+
+2)
+Enter a non-negative integer: 1000
+The reverse of 1000 is 1
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    return 0;
+}
